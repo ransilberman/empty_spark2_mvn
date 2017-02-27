f8881d3727f622d91b7dba2d94d4ef2032c0cf62
Ran Silberman <ran.silberman@feedvisor.com>
2017-02-27 19:02:59 +0200

    mllib

---
+++ child/src/main/scala/com/tikalk/LinearRegression.scala
@@ -0,0 +1,48 @@
+package com.tikalk
+
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql.SparkSession
+
+
+object LinearRegression {
+
+  def dorun(spark: SparkSession, logfile: String): Unit = {
+
+
+    val training = spark.read.format("libsvm")
+      .load(logfile)
+
+
+    val lr = new LinearRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+
+    // Fit the model
+    val lrModel = lr.fit(training)
+
+    // Print the coefficients and intercept for linear regression
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+
+    // Summarize the model over the training set and print out some metrics
+    val trainingSummary = lrModel.summary
+    trainingSummary.residuals.show()
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+  }
+
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder()
+      .master("local[2]") //TODO: remove this line and add to VM options: -Dspark.master=local[2]
+      .appName("SQL count")
+      .getOrCreate()
+
+    dorun(spark, "train2.txt")
+    spark.stop()
+  }
+
+}
--- child/src/main/scala/com/tikalk/LinearRegressionSparkML.scala
+++ child/src/main/scala/com/tikalk/LinearRegressionSparkML.scala
@@ -4,13 +4,13 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.ml.regression.LinearRegression
 
 
-object HelloWorldMl {
+object LinearRegressionSparkML {
 
   def dorun(spark: SparkSession, logfile: String): Unit = {
 
 
     val training = spark.read.format("libsvm")
-      .load("/home/rans/Downloads/train.txt")
+      .load(logfile)
 
 
     val lr = new LinearRegression()
@@ -24,11 +24,12 @@ object HelloWorldMl {
     // Print the coefficients and intercept for linear regression
     println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
 
+
     // Summarize the model over the training set and print out some metrics
     val trainingSummary = lrModel.summary
+    trainingSummary.residuals.show()
     println(s"numIterations: ${trainingSummary.totalIterations}")
     println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
-    trainingSummary.residuals.show()
     println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
     println(s"r2: ${trainingSummary.r2}")
   }
@@ -40,7 +41,7 @@ object HelloWorldMl {
       .appName("SQL count")
       .getOrCreate()
 
-    dorun(spark, "/home/rans/Downloads/train_1.csv")
+    dorun(spark, "train2.txt")
     spark.stop()
   }
 
